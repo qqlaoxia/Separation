f469afad982dad344e4fb9cc738e333f2614d8f0
Ray <xiamenglei321@163.com>
2020-09-04 14:54:01 +0800

    update20

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,6 @@ author = 'MTC'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
-pip install oe-sphinx-theme
 
 # -- General configuration ---------------------------------------------------
 
